3a1cd824e9029ad98c0a0b04c151dc5cf2f40e71
ABRARgomaa <abrargomaa111@gmail.com>
2024-07-12 01:31:54 +0300

    UP

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+-- Users table
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
