85c1ca21469623f92ad15b87d4ab1480f24cb894
ABRARgomaa <abrargomaa111@gmail.com>
2024-07-12 03:00:01 +0300

    up

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,11 +1,5 @@
--- table
-
-WITH COUNTRYBANDS AS(
-    SELECT origin, SUM(fans) AS nb_fans
-    FROM metal_bands
-    GROUP BY origin
-)
-
-SELECT origin, nb_fans
-FROM COUNTRYBANDS
+-- fans
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
 ORDER BY nb_fans DESC;
